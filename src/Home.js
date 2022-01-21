97cf3be2aba2239d23acbdf7fc82f83d0f514f2e
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-01-21 16:44:38 +0100

    useState example

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,27 @@
+import {useState} from 'react';
+
 const Home = () => {
-    
-    const handleClick = (e) => {
-        console.log('handling a Ninja Dynamic click', e);
+    // let name = 'Alexandre';
+    const [name, setName] = useState('Boeuf bourguignon')
+    const [length, setlength] = useState(`90'`)
+
+    const handleClick = () => {
+        // name = 'Marie';
+        // console.log(name);
+        setName('Ramen');
+        setlength(`45'`);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('handling a Ninja Dynamic click with parameter' + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('handling a Ninja Dynamic click with parameter' + name, e.target);
+    // }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}, {length} minutes</p>
             <button onClick={handleClick}>Click Me! </button>
-            <button onClick={(e) => handleClickAgain(' Alexandre', e)}>Click again! </button>
+            {/* <button onClick={(e) => handleClickAgain(' Alexandre', e)}>Click again! </button> */}
         </div>
      );
 }
