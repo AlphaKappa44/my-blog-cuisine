f0f83fc6b9e373af61858f3cb8a9c1a59dcbf280
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-01-25 20:42:06 +0100

    Router Link to replace <a href>

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Cuisine Web-Book</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New recipe</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New recipe</Link>
             </div>
         </nav>
      );
