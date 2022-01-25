8ba5fbcc689b0c357184e459b07943b713cda01c
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-01-25 21:37:47 +0100

    cleanUp function to cleanup incomplete use state (AbortController)

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -7,28 +7,46 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+
+        const abortController = new AbortController();
+
         // fetch the data as soon as the component renders
         setTimeout(() => {
-          fetch(url)
+
+          fetch(url, { signal: abortController.signal})
+
             .then((res) => {
+
               if (!res.ok) {
                 console.log(res);
                 throw new Error("Couldn't fetch data from server");
               }
+
               return res.json();
             })
+
             .then((data) => {
+
               console.log(data);
               setData(data);
               setIsLoading(false);
               setError(null);
             })
+
             .catch((err) => {
-              console.log(err.message);
-              setIsLoading(false);
-              setError(err.message);
+
+              if (err.name === "AbortError") {
+                console.log(err.name, err.message);
+
+              } else { 
+                setIsLoading(false);
+                setError(err.message);
+            }
             });
         }, 1000);
+        // cleanUp function to cleanup incomplete use state
+        return () => abortController.abort();
+          // console.log('cleanup incomplete use state');
       }, [url]);
 
       return {data, isLoading, error}
