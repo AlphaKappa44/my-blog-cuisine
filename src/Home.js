c27c3bf110e717168a4407fc10c44fafdb365107
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-01-25 01:25:53 +0100

    error state end rendering error in the browser

--- src/Home.js
+++ src/Home.js
@@ -1,65 +1,63 @@
-import { useState, useEffect } from 'react';
-import RecipeList from './RecipeList';
+import { useState, useEffect } from "react";
+import RecipeList from "./RecipeList";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [recipes, setRecipes] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  // const [title, setTitle] = useState('Boeuf Bourguignon');
 
-    // const [title, setTitle] = useState('Boeuf Bourguignon');
+  // const handleDelete = (id) => {
+  //     const deletedRecipe = recipes.filter(recipe => recipe.id !== id);
+  //     setRecipes(deletedRecipe);
+  // }
 
-    // const handleDelete = (id) => {
-    //     const deletedRecipe = recipes.filter(recipe => recipe.id !== id);
-    //     setRecipes(deletedRecipe);
-    // }
+  // useEffect with dependencies, triggers the effect from one state value.
+  // it will re-render the page only once with an arrayempty array passed in.
+  useEffect(() => {
+    // fetch the data as soon as the component renders
+    setTimeout(() => {
+      fetch("http://localhost:8000/recipesSSSSSSSSSSSSSSSSSS")
+        .then((res) => {
+          if (!res.ok) {
+            console.log(res);
+            throw new Error("Couldn't fetch data from server");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setRecipes(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
 
-    // useEffect with dependencies, triggers the effect from one state value.
-    // it will re-render the page only once with an arrayempty array passed in.
-    useEffect(() => {
-        
-        // fetch the data as soon as the component renders
-            setTimeout(() => {
-                fetch( 'http://localhost:8000/recipeSs')
-                    .then(res => {
-                        if (!res.ok) {
-                        console.log(res);
-                    throw new Error('Couldn\'t fetch data from server')}
-                        return res.json();
-                    })
-                    .then(data => {
-                        console.log(data);
-                        setRecipes(data);
-                        setIsLoading(false);
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                    })
-            }, 1000);
-   }, []);
+  return (
+    <div className="home">
+      { error && <h1 className="error"> { error } </h1> }
+      {isLoading && <div className="loading">Loading...</div>}
+      {recipes && (
+        <RecipeList
+          recipes={recipes}
+          title="Recipe List!"
+          // handleDelete={handleDelete}
+        ></RecipeList>
+      )}
 
-    return ( 
-
-        <div className="home">
-
-            {/* Evaluate the left side of  "&&" ("Recipes"), then if this is "false", right of "&&" is totally ignored.
-            Here it is ignored as it is null and null is evaluated as "false"  */}
-            {isLoading && <div className="loading">Loading...</div> }
-            { recipes && 
-            <RecipeList 
-                recipes={recipes} 
-                title="Recipe List!" 
-                // handleDelete={handleDelete}
-            >
-            </RecipeList>}
-
-            {/* <RecipeList recipes={recipes.filter((recipe) => recipe.title === 'Ramen')} title="Recipe List No2" ></RecipeList> 
+      {/* <RecipeList recipes={recipes.filter((recipe) => recipe.title === 'Ramen')} title="Recipe List No2" ></RecipeList> 
 
             <button onClick={() => setTitle('Tajine d\'agneau au fenouil')}>Change recipe's title</button>
             <p> {title} </p> */}
-            
+    </div>
+  );
+};
 
-        </div>
-     );
-}
- 
 export default Home;
